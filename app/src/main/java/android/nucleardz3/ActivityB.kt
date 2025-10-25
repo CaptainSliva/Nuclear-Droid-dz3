d3b9aed762bb628398bf97d547737b90f9a78a68
Captain Sliva <cripton2020@mail.ru>
2025-10-25 14:18:59 +0300

    refactor code and add launch activity when screen off

--- app/src/main/java/android/nucleardz3/ActivityB.kt
+++ app/src/main/java/android/nucleardz3/ActivityB.kt
@@ -15,31 +15,25 @@ import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import utils.functionsApp.keyIntentColor
 import kotlin.text.toHexString
 
 class ActivityB : AppCompatActivity() {
 
     private lateinit var binding: ActivityBBinding
-    val colorRestoreKey = "CEY_COLOR"
     var userColor = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-//
+
         binding = ActivityBBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userColor = intent.getIntExtra("color", 0)
+        userColor = intent.getIntExtra(keyIntentColor, 0)
         println("ucolor - {$userColor}")
-//
+
         if (userColor != 0) {
             binding.mainB.setBackgroundColor(userColor)
         }
@@ -57,19 +51,5 @@ class ActivityB : AppCompatActivity() {
         super.onResume()
         Log.i("Task stack", "${this.taskId}")
     }
-
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        outState.putInt(colorRestoreKey, userColor)
-//        super.onSaveInstanceState(outState)
-//    }
-//
-//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-//        super.onRestoreInstanceState(savedInstanceState)
-//
-//        userColor = savedInstanceState.getInt(colorRestoreKey)
-//        if (userColor != 0) {
-//            binding.mainB.setBackgroundColor(userColor)
-//        }
-//    }
 
 }
--- app/src/main/java/android/nucleardz3/ActivityC.kt
+++ app/src/main/java/android/nucleardz3/ActivityC.kt
@@ -1,16 +1,12 @@
 package android.nucleardz3
 
 import android.content.Intent
-import android.nucleardz3.databinding.ActivityBBinding
 import android.nucleardz3.databinding.ActivityCBinding
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.ui.AppBarConfiguration
 import kotlin.jvm.java
 
 class ActivityC : AppCompatActivity() {
@@ -20,12 +16,6 @@ class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
 
         binding = ActivityCBinding.inflate(layoutInflater)
@@ -33,7 +23,7 @@ class ActivityC : AppCompatActivity() {
 
         binding.bOpenA.setOnClickListener {
             finish()
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, ActivityA::class.java)
                 .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             this.startActivity(intent)
         }
